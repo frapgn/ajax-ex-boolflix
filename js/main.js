6802f2a440d236138eda4864fd4b1844170039b5
Francesco <59645305+frapgn@users.noreply.github.com>
2020-03-19 18:38:35 +0100

    ottimizzazione grafica e logica

--- js/main.js
+++ js/main.js
@@ -5,37 +5,50 @@ var filmBoxSrc = $('#film-box-template').html();
 var filmBoxTemplate = Handlebars.compile(filmBoxSrc);
 
 $('#search-button').click(function() {
-    var searchValue = $('#search').val();
-    $('#search').val('');
-    $('.film-box').remove();
+    ajaxFilms();
+});
 
-    $.ajax({
-        url: api_base_url + '/search/movie',
-        method: 'GET',
-        data: {
-            api_key: '75dbe3021ac7dc4530d9ca7b99004aa8',
-            query: searchValue,
-            language: 'it-IT'
-        },
-        success: function(res) {
-            console.log(res);
-            var films = res.results;
-            for (var i = 0; i < films.length; i++) {
-                var film = {
-                    title: films[i].title,
-                    originalTitle: films[i].original_title,
-                    originalLanguage: films[i].original_language,
-                    voteAverage: films[i].vote_average
+$('#search').keypress(function() {
+    if(event.key == 'Enter') {
+        ajaxFilms();
+    }
+});
+
+
+// Funzioni
+function ajaxFilms() {
+    if ($('#search').val() != '') {
+        var searchValue = $('#search').val();
+        $('#search').val('');
+        $('.film-box').remove();
+
+        $.ajax({
+            url: api_base_url + '/search/movie',
+            method: 'GET',
+            data: {
+                api_key: '75dbe3021ac7dc4530d9ca7b99004aa8',
+                query: searchValue,
+                language: 'it-IT'
+            },
+            success: function(res) {
+                console.log(res);
+                var films = res.results;
+                for (var i = 0; i < films.length; i++) {
+                    var film = {
+                        title: films[i].title,
+                        originalTitle: films[i].original_title,
+                        originalLanguage: films[i].original_language,
+                        voteAverage: films[i].vote_average
+                    }
+
+                    filmBoxTemplateHTML = filmBoxTemplate(film);
+                    $('.container').append(filmBoxTemplateHTML);
                 }
 
-                filmBoxTemplateHTML = filmBoxTemplate(film);
-                $('.container').append(filmBoxTemplateHTML);
+            },
+            error: function() {
+                console.log('Errore');
             }
-
-        },
-        error: function() {
-            console.log('Errore');
-        }
-    });
-
-});
+        });
+    }
+}

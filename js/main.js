7d4e3500873bff7cf10487096f98dfe3afa98d5a
Francesco <59645305+frapgn@users.noreply.github.com>
2020-03-22 20:48:46 +0100

    fix: gestione delle bandierine causava loop e non funzionava su chrome

--- js/main.js
+++ js/main.js
@@ -1,8 +1,9 @@
 
-
+// Template
 var boxTemplateSrc = $('#box-template').html();
 var boxTemplate = Handlebars.compile(boxTemplateSrc);
 
+// Ricerca col click e con Enter
 $('#search-button').click(function() {
     search();
 });
@@ -13,21 +14,20 @@ $('#search').keypress(function(event) {
     }
 });
 
+// Visualizza / Nascondi Descrizione
 $('.container').on('click', '#show-overview', function() {
     $(this).siblings('.overview-container').removeClass('hidden').addClass('flex');
-    console.log(this);
 });
 
 $('.container').on('click', '#hide-overview', function() {
     $(this).parent('.overview-container').removeClass('flex').addClass('hidden');
-    console.log(this);
 });
 
 // Funzioni
 function search() {
     var searchValue = $('#search').val();
     if ($('#search').val() != '') {
-        // $('#search').val('');
+        $('#search').val('');
         $('.box-container').remove();
         ajaxMovies(searchValue);
         ajaxTvShows(searchValue);
@@ -45,7 +45,6 @@ function ajaxMovies(searchValue) {
             language: 'it-IT'
         },
         success: function(res) {
-            // console.log(res);
             var movies = res.results;
              appendMovies(movies);
         },
@@ -66,7 +65,6 @@ function ajaxTvShows(searchValue) {
             language: 'it-IT'
         },
         success: function(res) {
-            // console.log(res);
             var tvShows = res.results;
              appendTvShows(tvShows);
         },
@@ -88,15 +86,16 @@ function appendMovies(movies) {
             poster: 'w342' + movies[i].poster_path,
             overview: movies[i].overview
         }
-
-        if (movie.originalLanguage == 'en') {
+        if (movie.originalLanguage == 'en') { // FIXME: non riesco a far funzionare l'if in una funzione
             movie.originalLanguage = 'gb';
         }
-
+        if (movie.overview == '') { // FIXME: non riesco a far funzionare l'if in una funzione
+            movie.overview = 'Descrizione non disponibile';
+        }
         boxTemplateHTML = boxTemplate(movie);
         $('.container .movies').append(boxTemplateHTML);
-
         posterNotAvailable();
+        imgNotAvailable(movie.originalLanguageUpperCase);
     }
 }
 
@@ -112,17 +111,25 @@ function appendTvShows(tvShows) {
             poster: 'w342' + tvShows[i].poster_path,
             overview: tvShows[i].overview
         }
-
-        if (tvShow.originalLanguage == 'en') {
+        if (tvShow.originalLanguage == 'en') { // FIXME: non riesco a far funzionare l'if in una funzione
             tvShow.originalLanguage = 'gb';
         }
-
+        if (tvShow.overview == '') { // FIXME: non riesco a far funzionare l'if in una funzione
+            tvShow.overview = 'Descrizione non disponibile';
+        }
         boxTemplateHTML = boxTemplate(tvShow);
         $('.container .tv-shows').append(boxTemplateHTML);
-
         posterNotAvailable();
+        imgNotAvailable(tvShow.originalLanguageUpperCase);
     }
+
 }
+
+// function flagENtoGB(originalLanguage) {
+//     if (originalLanguage == 'en') {
+//         originalLanguage = 'gb';
+//     }
+// }
 
 function posterNotAvailable() {
     $('.img-container figure img').on('error', function() {
@@ -131,6 +138,18 @@ function posterNotAvailable() {
     });
 }
 
+function imgNotAvailable(originalLanguageUpperCase) {
+    $('.original-language-container img').on('error', function() {
+        $(this).parent().append('<span class="text-instead-of-the-flag">' + originalLanguageUpperCase + '</span>');
+        $(this).remove();
+    });
+}
+
+// function overviewNotAvailable(overview) {
+//     if (overview.length === 0) {
+//         return overview = 'Descrizione non disponibile';
+//     }
+// }
 
 // ----------------------------------------------------------
 var greenTheme = $('head link[href*=green-theme]');
